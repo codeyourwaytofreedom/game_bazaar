5c76a6e2b76819c248206414b435231c21ae7626
codeyourwaytofreedom <can.kolej@gmail.com>
2023-07-31 23:47:09 +0300

    Items layout in place.
    Will add switch buttons.

--- components/Homie.tsx
+++ components/Homie.tsx
@@ -6,12 +6,14 @@ const Homie = () => {
         <div className={h.homie}>
             <div className={h.homie_wallpaper}></div>
             <div className={h.homie_banner}>Top Banner</div>
-            <div className={h.homie_categories}>
-                {
-                    [...Array(15)].map((item,index)=>
-                        <div>Item</div>
-                    )
-                }
+            <div className={h.homie_items}>
+                <div className={h.homie_items_shell}>
+                    {
+                        [...Array(24)].map((item,index)=>
+                            <div className={h.homie_items_shell_each}>Item</div>
+                        )
+                    }
+                </div>
             </div>
         </div>
     </> );
