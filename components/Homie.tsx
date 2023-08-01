17c1828aaac7917eb26fbee3882794719353b76f
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-01 12:36:02 +0300

    full url tested

--- components/Homie.tsx
+++ components/Homie.tsx
@@ -9,7 +9,7 @@ function getRandomNumber() {
 
 const Homie = () => {
     useEffect(()=>{
-        fetch("/api/hello").then(r=> r.json()).then(rj => console.log(rj))
+        fetch("https://game-bazaar.vercel.app/api/hello").then(r=> r.json()).then(rj => console.log(rj))
     },[])
     return ( <>
         <div className={h.homie}>
