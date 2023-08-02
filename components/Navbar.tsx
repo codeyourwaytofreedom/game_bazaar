da46eeeda78347ad10e64f5273b0e194662ab57b
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-02 21:56:10 +0300

    Mobile responsive updated.
    Redeploying

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,12 +10,12 @@ const Navbar = () => {
                 <Link href={"/"}><h2>Game Bazaar</h2></Link>
 
                 <Link href={"/"} id={h.login}>
-                    <Image src={"/login.png"} alt={"sword"} width={40} height={40}/>
-                    Log in with Steam
+                    <Image src={"/login.png"} alt={"sword"} width={30} height={30}/>
+                    <span>Log in with Steam</span>
                 </Link>
 
                 <Link id={h.comments} href={"/comments"}>
-                    <Image src={"/cmmt.png"} alt={"sword"} width={50} height={40}/>
+                    <Image src={"/cmmt.png"} alt={"sword"} width={30} height={30}/>
                 </Link>
             </div>
      );
