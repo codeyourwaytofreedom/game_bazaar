f6fb652486515f243c0716ad4007b73d4b6c6da7
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-18 16:29:42 +0300

    same stime prop added to cookie header.
    last try for cookie removal.
    
    will revisit

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { NextResponse,NextRequest } from 'next/server';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 
-export async function middleware(request: NextApiRequest) {
+export async function middleware(req: NextApiRequest) {
+  console.log("middleware working");
 /*   try {
     const referringURL = request.headers.referer;
     const queryParams = new URLSearchParams(referringURL);
@@ -23,5 +24,5 @@ export async function middleware(request: NextApiRequest) {
 }
 
 export const config = {
-  matcher: '/market/tm2/:path*',
+  matcher: '/',
 }
--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -7,27 +7,7 @@ export default async function handler(
 ) {
     console.log("Log out request received");
 
-    const idCookie = req.cookies.ID;
-    let steamID;
+    res.setHeader('Set-Cookie', 'ID=; HttpOnly; Max-Age=0; Path=/; Secure; SameSite=None');
+    res.status(200).send('OK');
 
-    if(idCookie){
-        const isSteamOpenIDURL = idCookie.includes("https://steamcommunity.com/openid/id/");
-        if(isSteamOpenIDURL){
-            const parts = idCookie.split("/");
-            steamID = parts[parts.length - 1];
-        }
-        else {
-            steamID = idCookie;
-        }
-    }
-    console.log(steamID);
-
-    if(steamID){
-      res.setHeader('Set-Cookie', 'ID=""; HttpOnly; Max-Age=0; Path=/; Secure');
-      res.status(200).send('OK');
-    }
-    else{
-      res.setHeader('Set-Cookie', 'ID=""; HttpOnly; Max-Age=0; Path=/; Secure');
-      res.status(200).send('OK');
-    }
 }
