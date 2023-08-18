26d130cd224378fbc4fe2fa0e20daefd225c08a1
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-18 16:34:59 +0300

    caching disabled.....

--- pages/api/ins.ts
+++ pages/api/ins.ts
@@ -24,7 +24,9 @@ export default async function handler(
         const url = existingUser.profile_img_url;
         const balance = existingUser.balance;
         const id = existingUser.steamId;
-        console.log(balance)
+        console.log(balance);
+        
+        res.setHeader('Cache-Control', 'no-store');
         res.setHeader('Set-Cookie', `ID=${id}; HttpOnly; Max-Age=${60 * 60}; Path=/; Secure`);
         res.status(200).json({url:url,id:id, balance:balance});
       }
@@ -57,6 +59,7 @@ export default async function handler(
                   { upsert: true }
                 );
 
+                res.setHeader('Cache-Control', 'no-store');
                 res.setHeader('Set-Cookie', `ID=${steamID}; HttpOnly; Max-Age=${60 * 60}; Path=/; Secure`);
                 res.status(200).json({url:profileImage,id:steamID, balance:0});
                 } else {
