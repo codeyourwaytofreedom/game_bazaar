a7bcd8a53773195d6a4751cf9395ecc4c136c4c6
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-18 16:17:40 +0300

    ....id searching added to logout route

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -6,7 +6,28 @@ export default async function handler(
   res: NextApiResponse
 ) {
     console.log("Log out request received");
-    res.setHeader('Set-Cookie', 'ID=""; HttpOnly; Max-Age=0; Path=/; Secure');
-    res.status(200).send('OK');
 
+    const idCookie = req.cookies.ID;
+    let steamID;
+
+    if(idCookie){
+        const isSteamOpenIDURL = idCookie.includes("https://steamcommunity.com/openid/id/");
+        if(isSteamOpenIDURL){
+            const parts = idCookie.split("/");
+            steamID = parts[parts.length - 1];
+        }
+        else {
+            steamID = idCookie;
+        }
+    }
+    console.log(steamID);
+
+    if(steamID){
+      res.setHeader('Set-Cookie', 'ID=""; HttpOnly; Max-Age=0; Path=/; Secure');
+      res.status(200).send('OK');
+    }
+    else{
+      res.setHeader('Set-Cookie', 'ID=""; HttpOnly; Max-Age=0; Path=/; Secure');
+      res.status(200).send('OK');
+    }
 }
