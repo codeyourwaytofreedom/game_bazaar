7436d59d2f3f4492098f13d64f8d6b8f0a4dd421
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-21 17:06:41 +0300

    ....isSafe system in place...

--- pages/api/ins.ts
+++ pages/api/ins.ts
@@ -20,7 +20,7 @@ export default async function handler(
       const existingUser = await members.findOne({ steamId: id });
 
       if(existingUser){
-        console.log("Mevcut user dan devam etti")
+        console.log("Mevcut user dan devam etti");
         const url = existingUser.profile_img_url;
         const balance = existingUser.balance;
         const id = existingUser.steamId;
@@ -30,43 +30,59 @@ export default async function handler(
         res.status(200).json({url:url,id:id, balance:balance});
       }
       else{
-        console.log("0 dan ekledi")
-        const apiUrl = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM}&steamids=${id}`;
-        await fetch(apiUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                if (
-                data.response &&
-                data.response.players &&
-                data.response.players.length > 0 &&
-                data.response.players[0].avatarfull
-                ) {
-                const profileImage = data.response.players[0].avatarfull;
-
-                  members.updateOne(
-                  { steamId: id },
-                  { $setOnInsert: { 
-                      steamId: id, 
-                      profile_img_url:profileImage,
-                      date_joined: new Date(),
-                      balance:0,
-                      delivery_time:"12 hr",
-                      email:"",
-                      steam_api_key:"keykey",
-                      game_bazaar_api_key:"...."
-                    } }, 
-                  { upsert: true }
-                );
-
-                res.setHeader('Set-Cookie', `ID=${steamID}; HttpOnly; Max-Age=${60 * 60}; Path=/; Secure`);
-                res.status(200).json({url:profileImage,id:steamID, balance:0});
-                } else {
-                console.log('Profile image not found');
-                }
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+        console.log("0 dan ekledi");
+        try{
+          const IsSafe = await fetch(`https://rep.autobot.tf/json/${id}`);
+          const resJson = await IsSafe.json();
+          const isSafe = !resJson.isBanned;
+          console.log("isSafe : ",isSafe)
+          if(isSafe){
+            const apiUrl = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM}&steamids=${id}`;
+            await fetch(apiUrl)
+                .then((response) => response.json())
+                .then((data) => {
+                    if (
+                    data.response &&
+                    data.response.players &&
+                    data.response.players.length > 0 &&
+                    data.response.players[0].avatarfull
+                    ) {
+                    const profileImage = data.response.players[0].avatarfull;
+    
+                      members.updateOne(
+                      { steamId: id },
+                      { $setOnInsert: { 
+                          steamId: id, 
+                          profile_img_url:profileImage,
+                          date_joined: new Date(),
+                          balance:0,
+                          delivery_time:"12 hr",
+                          email:"",
+                          steam_api_key:"keykey",
+                          game_bazaar_api_key:"...."
+                        } }, 
+                      { upsert: true }
+                    );
+    
+                    res.setHeader('Set-Cookie', `ID=${steamID}; HttpOnly; Max-Age=${60 * 60}; Path=/; Secure`);
+                    res.status(200).json({url:profileImage,id:steamID, balance:0});
+                    } else {
+                    console.log('Profile image not found');
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+          }
+          else{
+            console.log("User is banned")
+            res.status(403).json({ error: 'User is not trusted !!!' });
+          }
+        }
+        catch(error){
+          console.log(error);
+          res.status(403).json({ error: 'Could not verify if user is trusted !!!' });
+        }
       }
     }
 
