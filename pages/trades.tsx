d2a1cae5f528d28e3c0cbab76dfe721e14848972
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-28 12:30:38 +0300

    ...trades page demo done...

--- pages/trades.tsx
+++ pages/trades.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import t from "../styles/Pages.module.css";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Trades = () => {
     const formatter = (price:string) => {
@@ -17,12 +17,27 @@ const Trades = () => {
         }
     }
     const [chosen, setChosen] = useState(-1);
+    const [transactions, setTransactions] = useState<any>();
 
     const tabs = ["All transactions", "Completed", "Pending", "Failed"]
-    const actions = [{status:"All transactions", icon:"⛓", color:"silver"},{status:"Completed", icon:"★", color:"lightgreen"}, {status:"Pending", icon:"♨", color:"gold"},{status:"Failed", icon:"☓", color:"crimson"}]
+    const actions = [
+                        {status:"All transactions", icon:"|||", color:"skyblue", date:"06.09.2023 14:47" },
+                        {status:"Completed", icon:"★", color:"lightgreen",date:"06.09.2023 14:47"}, 
+                        {status:"Pending", icon:"♨", color:"gold",date:"06.09.2023 14:47"},
+                        {status:"Failed", icon:"☓", color:"crimson",date:"06.09.2023 14:47"}
+                    ]
 
     let dummy:any = [];
     [...Array(15)].map(e=> dummy.push(actions.slice(1,4)[Math.floor(Math.random() * 3)]));
+
+    useEffect(()=>{
+        if(chosen === 0){
+            setTransactions(dummy)
+        }
+        else{
+            setTransactions(dummy.filter((e:any)=> e.status === actions[chosen].status));
+        }
+    },[chosen])
 
     return ( 
         
@@ -37,12 +52,12 @@ const Trades = () => {
                 </div>
                 <div className={t.trades_all}>
                     {
-                        dummy.map((e:any,i:any)=>
-                            <div className={t.trades_all_each} style={{background:i%2 ? "#36454F" : "#13242f"}} key={i}>
+                        transactions && transactions.map((e:any,i:any)=>
+                            <div className={t.trades_all_each} style={{background:i%2 ? "#36454F" : "#13242f"}} key={i} suppressHydrationWarning>
                                <span suppressHydrationWarning style={{color:e.color, fontSize:"x-large", paddingRight:"40px"}}>{e.icon}</span>
                                <div id={t.name} style={{color:e.color}}>
                                     <span>Item Name goes here...</span>
-                                    <span style={{fontSize:"small"}}>{new Date().toLocaleDateString()} {new Date().toLocaleTimeString()}</span>
+                                    <span style={{fontSize:"small"}} suppressHydrationWarning>{e.date}</span>
                                </div>
                                <div id={t.image}>
                                     <span style={{boxShadow:`0 0 35px 14px ${e.color}`}}></span>
