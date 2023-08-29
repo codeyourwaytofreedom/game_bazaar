d99048c7b70adf339bd75058098e3ede7aca67df
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-29 18:39:57 +0300

    testing post request

--- pages/api/fetch_inventory.ts
+++ pages/api/fetch_inventory.ts
@@ -11,16 +11,17 @@ const cors = Cors({
 cors(handler as any);
 
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
-
-
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 
     console.log("DOC- Fetch Inventory");
-    res.status(200).json({test:"Emre bey bu metni görüyorsanız CORS çalışıyor demektir"})
+
+    const input = JSON.parse(req.body).number;
+    const int_input = parseInt(input);
+    const square = int_input*int_input;
+
+    console.log(square)
+
+
+    res.status(200).json({result:square})
 
 }
