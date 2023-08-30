fc7ca86745a49cf2fdb1bdbd04b8ba661628ca3d
codeyourwaytofreedom <can.kolej@gmail.com>
2023-08-30 13:17:13 +0300

    middleware for protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,24 @@
 import { NextResponse,NextRequest } from 'next/server';
-import { NextApiRequest, NextApiResponse } from 'next';
 
 
-export async function middleware(req: NextApiRequest, res:NextApiResponse) {
-  //console.log("middleware working", req.cookies);
-/*   try {
-    const referringURL = request.headers.referer;
-    const queryParams = new URLSearchParams(referringURL);
-    const steamID = queryParams.get('openid.identity');
+export async function middleware(req: NextRequest, res:NextResponse) {
 
-    if (!steamID) {
-        return NextResponse.redirect(new URL("/", request.url));
+  try {
+    const ID = req.cookies.get('ID')?.value;
+    if(!ID){
+      return NextResponse.redirect(new URL("/", req.url));
     }
 
   } catch (error) {
     console.log(error)
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   const response = NextResponse.next()
-  return response */
+  return response
 
 }
 
 export const config = {
-  matcher: '/:path*',
-}
+  matcher: '/(inventory|profile|balance|trades|market/:path*)',
+};
