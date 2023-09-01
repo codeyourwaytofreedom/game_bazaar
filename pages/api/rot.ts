d691056e576a0a4614afc18b447ecde3500bec0a
codeyourwaytofreedom <can.kolej@gmail.com>
2023-09-01 11:25:51 +0300

    ....rot end point

--- pages/api/rot.ts
+++ pages/api/rot.ts
@@ -1,26 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { HttpsProxyAgent } from 'https-proxy-agent';
-import fetch from 'node-fetch';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    //With proxy agent
-    const agent = new HttpsProxyAgent('http://200.105.215.22:33630');
-    try {
-        const response = await fetch('http://httpbin.org/ip', { agent });
-        const body = await response.text();
-        console.log(body);
-    } catch (error) {
-        //console.log('error', error);
-    }
 
-    //without proxy agent
-    try {
-        const response = await fetch('http://httpbin.org/ip');
-        const body = await response.text();
-        console.log(body);
-    } catch (error) {
-        //console.log('error', error);
-    }
+    console.log("Fast api test route")
+    try{        
+        const response = await fetch('https://rotator-delta.vercel.app/api/python?key=victoria&id=66_88');
+        const status = response.status;
 
-  res.send("OK")
+        if(status === 200){
+            const resJson = await response.json();
+            console.log(resJson.key, resJson.id);
+            res.status(200).json({message:status})
+        }
+
+    }
+    catch(err){
+        console.log(err)
+    }
 }
