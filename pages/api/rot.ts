ba19dc069b12a62d00bf4b76924dfc9dc16c2e47
codeyourwaytofreedom <can.kolej@gmail.com>
2023-09-01 15:47:08 +0300

    ....get trade offers worked..

--- pages/api/rot.ts
+++ pages/api/rot.ts
@@ -2,19 +2,22 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
+    const url = `https://api.steampowered.com/IEconService/GetTradeOffers/v1?key=${process.env.STEAM}&get_sent_offers=true&get_received_offers=true&get_descriptions=true&language=en&active_only=false&historical_only=false&time_historical_cutoff=0`
+
     console.log("Fast api test route")
     try{        
-        const response = await fetch('https://rotator-delta.vercel.app/api/python?key=victoria&id=66_88');
+        const response = await fetch(url);
         const status = response.status;
+        const resJson = await response.json();
 
-        if(status === 200){
-            const resJson = await response.json();
-            console.log(resJson.key, resJson.id);
-            res.status(200).json({message:status})
-        }
+        console.log(resJson.response);
+
+        res.send("OK")
+
 
     }
     catch(err){
         console.log(err)
+        res.send("problem")
     }
 }
