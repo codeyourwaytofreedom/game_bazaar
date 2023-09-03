9932a2593ac8b743920b043bc36ce974d2fad4e8
codeyourwaytofreedom <can.kolej@gmail.com>
2023-09-03 22:58:49 +0300

    ...cron job re-test

--- pages/api/cron/cron.ts
+++ pages/api/cron/cron.ts
@@ -1,12 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import cron from 'node-cron';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
     console.log("Hello Cron")
-    cron.schedule('*/5 * * * * *', () => {
-        console.log('Cron job is running every 5 seconds.');
-    });
 }
