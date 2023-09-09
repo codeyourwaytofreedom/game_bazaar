37ea84de463ffddea31d2dff37703015def54704
codeyourwaytofreedom <can.kolej@gmail.com>
2023-09-09 22:08:13 +0300

    dynamic appid added...

--- pages/api/cron/check_trade.ts
+++ pages/api/cron/check_trade.ts
@@ -274,15 +274,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                       console.log(seller_trade_state,buyer_trade_state)
                     }
 
+                    //Olumlu trade ancak to_be_given undefined
                     if(to_be_received && !to_be_given){
                       const buyer_trade_state = buyer_data.trade_offers.find((order:any)=> order.pure_id === seller_id).trade_offer_state;
                       //const seller_trade_state = seller_data.trade_offers.find((order:any)=> order.pure_id === buyer_id).trade_offer_state;
                       if(buyer_trade_state === 3){
+                        console.log("3-3 devam edilebilir");
                         let seller_inventory_new = await fetch_new_inventory(seller_id,seller_steam_api_key,appId);
-                        const seller_inventory_old = seller?.descriptions_440;
+                        const seller_inventory_old = seller?.[`descriptions_${appId}`];
 
                         const buyer_inventory_new = await fetch_new_inventory(buyer_id,buyer_steam_api_key,appId);
-                        const buyer_inventory_old = buyer?.descriptions_440;
+                        const buyer_inventory_old = buyer?.[`descriptions_${appId}`];
 
                         //transferring existing prices from old inventory in game bazaar
                         //match according to market_hash_name
@@ -297,6 +299,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                             });
                           });
                         }
+
                         //transferring existing prices from old inventory in game bazaar
                         seller_inventory_old.forEach((old_element:any) => {
                           if(old_element.assetid === order_assetid){
@@ -305,7 +308,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                           }
                             if(seller_inventory_new){
                               seller_inventory_new.forEach((new_element:any) => {
-                                if(old_element.market_hash_name === new_element.market_hash_name)
+                                if(old_element.assetid === new_element.assetid)
                                 {
                                     new_element.price = old_element.price
                                 }
@@ -316,193 +319,72 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                               seller_inventory_new = [];
                             }
                           });
-                        console.log(seller_inventory_old.length);
-                        console.log(seller_inventory_new.length);
 
-                        console.log(buyer_inventory_old.length);
-                        console.log(buyer_inventory_new.length);
+                          console.log(seller_inventory_old.length);
+                          console.log(seller_inventory_new.length);
+  
+                          console.log(buyer_inventory_old.length);
+                          console.log(buyer_inventory_new.length);
 
-                        const response_seller = await members.updateOne(
-                          {
-                            steamId: seller_id,
-                            they_ordered: {
-                              $elemMatch: {
-                                sellerId: seller_id,
-                                buyer_id: buyer_id,
-                                assetid: order.assetid,
-                                when:order.when
+
+                          const response_seller = await members.updateOne(
+                            {
+                              steamId: seller_id,
+                              they_ordered: {
+                                $elemMatch: {
+                                  sellerId: seller_id,
+                                  buyer_id: buyer_id,
+                                  assetid: order.assetid,
+                                  when:order.when
+                                }
                               }
+                            },
+                            {
+                              $set: {
+                                "they_ordered.$.status": "Completed",
+                                "they_ordered.$.transaction_closed_at": new Date(),
+                                [`descriptions_${appId}`]:seller_inventory_new,
+                                [`last_updated_${appId}`]:new Date()
+                              },
+                              $inc: {
+                                "balance": price,
+                              },
                             }
-                          },
-                          {
-                            $set: {
-                              "they_ordered.$.status": "Completed",
-                              descriptions_440:seller_inventory_new,
-                              last_updated_440:new Date()
-                            },
-                            $inc: {
-                              "balance": price,
-                            },
-                          }
-                        );
-
-                        const response_buyer = await members.updateOne(
-                          {
-                            steamId: buyer_id,
-                            I_ordered: {
-                              $elemMatch: {
-                                sellerId: seller_id,
-                                buyer_id: buyer_id,
-                                assetid: order.assetid,
-                                when:order.when
+                          );
+  
+                          const response_buyer = await members.updateOne(
+                            {
+                              steamId: buyer_id,
+                              I_ordered: {
+                                $elemMatch: {
+                                  sellerId: seller_id,
+                                  buyer_id: buyer_id,
+                                  assetid: order.assetid,
+                                  when:order.when
+                                }
                               }
+                            },
+                            {
+                              $set: {
+                                "I_ordered.$.status": "Completed",
+                                "I_ordered.$.transaction_closed_at": new Date(),
+                                [`descriptions_${appId}`]:buyer_inventory_new,
+                                [`last_updated_${appId}`]:new Date()
+                              },
+                              $inc: {
+                                "balance": -price,
+                              },
                             }
-                          },
-                          {
-                            $set: {
-                              "I_ordered.$.status": "Completed",
-                              descriptions_440:buyer_inventory_new,
-                              last_updated_440:new Date()
-                            },
-                            $inc: {
-                              "balance": -price,
-                            },
-                          }
-                        );
+                          );
+  
+                         console.log(response_seller.modifiedCount, response_buyer.modifiedCount,"Bingo, go check balances") 
 
-                       console.log(response_seller.modifiedCount, response_buyer.modifiedCount,"Bingo, go check balances") 
 
                       }
                       
                     }
 
-
-/*                     items_to_give.forEach((item_to_give:any) => {
-                        items_to_receive.forEach(async (item_to_receive:any) => {
-                            if(     _.isEqual(item_to_give,item_to_receive) 
-                                    && item_to_give.assetid === order_assetid 
-                                    && item_to_receive.assetid === order_assetid )
-                            {
-                                const seller_trade_state = seller_data.trade_offers.find((order:any)=> order.pure_id === buyer_id).trade_offer_state;
-                                const buyer_trade_state = buyer_data.trade_offers.find((order:any)=> order.pure_id === seller_id).trade_offer_state;
-                                if(seller_trade_state === 3 && buyer_trade_state === 3){
-                                    //console.log("Execute balance ops");
-                                    console.log(item_to_receive.assetid);
-                                    console.log(item_to_give.assetid);
-                                    //console.log(order_assetid);
-
-                                    //change 440 to dynamic later on updating order object
-
-                                    let seller_inventory_new = await fetch_new_inventory(seller_id,seller_steam_api_key,appId);
-                                    const seller_inventory_old = seller?.descriptions_440;
-
-                                    const buyer_inventory_new = await fetch_new_inventory(buyer_id,buyer_steam_api_key,appId);
-                                    const buyer_inventory_old = buyer?.descriptions_440;
-
-
-                                    //transferring existing prices from old inventory in game bazaar
-                                    //match according to market_hash_name
-                                    if(buyer_inventory_old){
-                                      buyer_inventory_old.forEach((old_item:any) => {
-                                        buyer_inventory_new.forEach((new_item:any) => {
-                                            if(old_item.assetid === new_item.assetid){
-                                                new_item.price = old_item.price
-                                            }else{
-                                              new_item.price = 0
-                                            }
-                                        });
-                                      });
-                                    }
-
-
-                                      //transferring existing prices from old inventory in game bazaar
-                                      seller_inventory_old.forEach((old_element:any) => {
-                                        if(old_element.assetid === order_assetid){
-                                            old_element.price = 0;
-                                            //buyer_inventory_new.push(old_element);
-                                        }
-                                          if(seller_inventory_new){
-                                            seller_inventory_new.forEach((new_element:any) => {
-                                              if(old_element.market_hash_name === new_element.market_hash_name)
-                                              {
-                                                  new_element.price = old_element.price
-                                              }
-                                            });
-                                          }
-                                          else{
-                                            console.log("No new inventory for the seller!");
-                                            seller_inventory_new = [];
-                                          }
-                                        });
-                                    
-                                    
-                                    //console.log(seller_inventory_old.length);
-                                    //console.log(seller_inventory_new.length);
-
-                                    //console.log(buyer_inventory_old.length);
-                                    //console.log(buyer_inventory_new.length);
-                                    
-                                    const response_seller = await members.updateOne(
-                                        {
-                                          steamId: seller_id,
-                                          they_ordered: {
-                                            $elemMatch: {
-                                              sellerId: seller_id,
-                                              buyer_id: buyer_id,
-                                              assetid: order.assetid,
-                                              when:order.when
-                                            }
-                                          }
-                                        },
-                                        {
-                                          $set: {
-                                            "they_ordered.$.status": "Completed",
-                                            descriptions_440:seller_inventory_new,
-                                            last_updated_440:new Date()
-                                          },
-                                          $inc: {
-                                            "balance": price,
-                                          },
-                                        }
-                                      );
-
-                                      const response_buyer = await members.updateOne(
-                                        {
-                                          steamId: buyer_id,
-                                          I_ordered: {
-                                            $elemMatch: {
-                                              sellerId: seller_id,
-                                              buyer_id: buyer_id,
-                                              assetid: order.assetid,
-                                              when:order.when
-                                            }
-                                          }
-                                        },
-                                        {
-                                          $set: {
-                                            "I_ordered.$.status": "Completed",
-                                            descriptions_440:buyer_inventory_new,
-                                            last_updated_440:new Date()
-                                          },
-                                          $inc: {
-                                            "balance": -price,
-                                          },
-                                        }
-                                      );
-
-                                     console.log(response_seller.modifiedCount, response_buyer.modifiedCount,"Bingo, go check balances")   
-                                }
-                                else{
-                                    console.log("Not yet")
-                                }
-                            }
-                            else{
-                                console.log("Triple match failed")
-                            }
-                        });
-                    }); */
-
-
+                    //Olumsuz sonuç
 /*                     if(!to_be_given){
                         console.log("Gönderici Göndermedi");
                         //console.log(order_assetid, seller_id, buyer_id)
@@ -521,7 +403,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                             },
                             {
                               $set: {
-                                "they_ordered.$.status": "Failed"
+                                "they_ordered.$.status": "Failed",
+                                "they_ordered.$.transaction_closed_at": new Date(),
                               }
                             }
                           );                          
@@ -541,13 +424,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                             {
                               $set: {
                                 "I_ordered.$.status": "Failed",
+                                "I_ordered.$.transaction_closed_at": new Date(),
                               },
                             }
                           );
                           console.log(response_seller.modifiedCount,response_buyer.modifiedCount,"Failed trade");
                     } */
 
-                    //console.log(match)
                 }
                 else{
                     //console.log(order.status)
